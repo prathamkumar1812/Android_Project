4eb65bb582bba9059ce71f35d2eebdea75450ce5
prathamkumar1812 <prathamkumar.mbd@gmail.com>
2023-01-31 16:06:34 +0530

    made somechanges in layout file and also change the Primary color value

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,23 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
    TextView textView;
+   Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.textView);
         textView.setText("hellow");
-<<<<<<< HEAD
-dhkdhdshd
-=======
-        textView.setText("game");
+        button=findViewById(R.id.button);
 
->>>>>>> master
 
+
+       // textView.setText("game");
+
+
+
+
+    }
+
+    public void clickplease(View view) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Start the Project");
+            }
+        });
 
     }
 }
