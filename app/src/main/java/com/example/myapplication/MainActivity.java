99d5433af48228c68372935d911a4b89779faeff
prathamkumar1812 <prathamkumar.mbd@gmail.com>
2023-01-31 14:07:50 +0530

    made somechanges

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.textView);
+        textView.setText("hellow");
 
 
 
